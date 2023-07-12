5f3ab5b6b96d7780ad13c7b056a4fa70c3edb07a
gavriel jaffe <gavrieljaffe@gmail.com>
2023-07-12 16:42:13 +0300

    the base for leetcode

---
+++ leetcode1.py
@@ -0,0 +1,23 @@
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         pass
+
+
+def twoSum(nums,target):
+    for i in range(len(nums)):
+        # print('i',i)
+        print('nums[i]', nums[i])
+        for j in range(len(nums)):
+            if nums[i] != nums[j]:
+                if n[i] + 
+
+
+def main (): 
+    nums = [7,11,2,15] 
+    target = 9 
+
+    twoSum(nums,target)
+    pass
+
+if __name__ == '__main__':
+    main()
