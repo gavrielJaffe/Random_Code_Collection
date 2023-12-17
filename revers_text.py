1bd5c291975adc5e0272c0b78358319dc220272c
gavrielJaffe <gavrielmn@gmail.com>
2023-12-17 17:00:24 +0200

    revers any string

--- revers_text.py
+++ revers_text.py
@@ -3,8 +3,14 @@
 def revers_text(text):
     return text[::-1]
 
+def revers_text_using_str(text):
+    new_text = ""
+    for char in text:
+        new_text = new_text + char
+    return new_text
+
 def main():
-    print(revers_text("olleh"))
+    print(revers_text_using_str("olleh"))
 
 if __name__ == '__main__':
     main()
